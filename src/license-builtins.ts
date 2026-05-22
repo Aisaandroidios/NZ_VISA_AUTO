d118a617504f1f95cfc16511b41f5a96e614b8c3
Jeckson <huhaiaisa@gmail.com>
2026-05-22 01:17:02 -0400

    COMMIT 0.1.0

---
+++ src/license-builtins.ts
@@ -0,0 +1,5 @@
+export const builtInLicensePublicKeySha256 =
+  "173fbe0d0197d6444aaa8134f8feb767b2a5ced862809ac5f53b34ab8e7b96f5";
+
+export const builtInLicenseGuardSalt =
+  "c0a198820c87423e82669c875e7ad3ab";
